be10e3e0950ad68f6ed0bb16dbad6f8e56e26655
Divyanshu Maithani <div.blackcat@gmail.com>
2019-08-28 19:57:22 +0530

    Add stuff and links

--- build.js
+++ build.js
@@ -10,53 +10,52 @@ const path = require('path')
 const options = {
   padding: 1,
   margin: 1,
-  borderStyle: 'round'
 }
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('               Tierney Cyren'),
-  handle: chalk.white('bitandbang'),
-  work: chalk.white('Senior Cloud Developer Advocate at Microsoft'),
-  opensource: chalk.white('Node.js Community Committee ') + chalk.green('⬢'),
-  twitter: chalk.gray('https://twitter.com/') + chalk.cyan('bitandbang'),
-  npm: chalk.gray('https://npmjs.com/') + chalk.red('~bnb'),
-  github: chalk.gray('https://github.com/') + chalk.green('bnb'),
-  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('bitandbang'),
-  web: chalk.cyan('https://bnb.im'),
-  npx: chalk.red('npx') + ' ' + chalk.white('@bnb/card  (via GitHub Package Registry)'),
+  name: chalk.white.bold('           Divyanshu Maithani'),
+  handle: chalk.white('@divyanshu013'),
+  work: chalk.white('Senior Software Engineer @Instawork'),
+  opensource: chalk.cyan(`{...JavaScript} engineer`) + chalk.white('. I love open-source!      '),
+  twitter: chalk.gray('https://twitter.com/') + chalk.cyan('divyanshu013'),
+  npm: chalk.gray('https://npmjs.com/') + chalk.red('~divyanshu013'),
+  github: chalk.gray('https://github.com/') + chalk.green('divyanshu013'),
+  web: chalk.cyan('https://divyanshu013.dev'),
+  stackoverflow: chalk.gray('https://stackoverflow.com/users/') + chalk.magenta('4952669/divyanshu-maithani'),
+  labelStackoverflow: chalk.white.bold('         SO:'),
+  pastWork: chalk.white(' Senior Frontend Engineer @appbaseio'),
+  labelPastwork: chalk.white.bold('       Past:'),
   labelWork: chalk.white.bold('       Work:'),
-  labelOpenSource: chalk.white.bold('Open Source:'),
+  labelOpenSource: chalk.white.bold('        Bio:'),
   labelTwitter: chalk.white.bold('    Twitter:'),
   labelnpm: chalk.white.bold('        npm:'),
   labelGitHub: chalk.white.bold('     GitHub:'),
-  labelLinkedIn: chalk.white.bold('   LinkedIn:'),
-  labelWeb: chalk.white.bold('        Web:'),
-  labelCard: chalk.white.bold('       Card:')
+  labelWeb: chalk.white.bold('       Blog:'),
 }
 
 // Actual strings we're going to output
 const newline = '\n'
 const heading = `${data.name} / ${data.handle}`
 const working = `${data.labelWork}  ${data.work}`
+const pastWorking = `${data.labelPastwork} ${data.pastWork}`
 const opensourcing = `${data.labelOpenSource}  ${data.opensource}`
 const twittering = `${data.labelTwitter}  ${data.twitter}`
 const npming = `${data.labelnpm}  ${data.npm}`
 const githubing = `${data.labelGitHub}  ${data.github}`
-const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
 const webing = `${data.labelWeb}  ${data.web}`
-const carding = `${data.labelCard}  ${data.npx}`
+const stackoverflow = `${data.labelStackoverflow}  ${data.stackoverflow}`
 
 // Put all our output together into a single variable so we can use boxen effectively
 const output = heading + // data.name + data.handle
                newline + newline + // Add one whole blank line
                working + newline + // data.labelWork + data.work
+               pastWorking + newline +
                opensourcing + newline + newline + // data.labelOpenSource + data.opensource
-               twittering + newline + // data.labelTwitter + data.twitter
-               npming + newline + // data.labelnpm + data.npm
-               githubing + newline + // data.labelGitHub + data.github
-               linkedining + newline + // data.labelLinkedIn + data.linkedin
                webing + newline + newline + // data.labelWeb + data.web
-               carding // data.labelCard + data.npx
-
+               githubing + newline + // data.labelGitHub + data.github
+               stackoverflow + newline +
+               twittering + newline + // data.labelTwitter + data.twitter
+               npming + newline; // data.labelnpm + data.npm
+               
 fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.green(boxen(output, options)))
